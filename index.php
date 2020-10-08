21df98c15eb2302b49478026dff651adcda35de5
Matthis HOULES <48548675+dyltha@users.noreply.github.com>
2020-10-08 12:45:51 +0200

    update routes

--- index.php
+++ index.php
@@ -25,6 +25,34 @@ $Router = new Router;
  * $router->add(URL, ['controller' => ControllerPage@MethodName]);
  */
 $Router->add('/php/category', ['controller' => 'CategoryC@displayPage']);
+$Router->add('/php/editcategory', ['controller' => 'EditCategoryC@displayPage']);
+$Router->add('/php/createcategory', ['controller' => 'CreateCategoryC@displayPage']);
+
+// mail
+$Router->add('/php/addmail', ['controller' => 'MailC@displayPage']);
+
+// fav & blog 
+$Router->add('/php/fav', ['controller' => 'CategoryC@favPage']);
+$Router->add('/php/blog', ['controller' => 'BlogC@displayPage']);
+
+
+// article
+$Router->add('/php/article', ['controller' => 'ArticleC@displayPage']);
+
+
+// user
+$Router->add('/php/connexion', ['controller' => 'UserC@signIn']);
+$Router->add('/php/inscription', ['controller' => 'UserC@signUp']);
+$Router->add('/php/editerprofil', ['controller' => 'UserC@editProfile']);
+$Router->add('/php/changepwdm', ['controller' => 'UserC@changePasswordMail']);
+$Router->add('/php/changepwdc', ['controller' => 'UserC@changePasswordCode']);
+$Router->add('/php/validation', ['controller' => 'UserC@validateAccount']);
+$Router->add('/php/homepage',['controller' => 'UserHomePageC@DisplayUserPage']); 
+$Router->add('/php/deconnexion', ['controller' => 'SignOutC@signOut']);
+
+// admin
+$Router->add('/php/myblog', ['controller' => 'BlocC@displayMyBlog']);
+$Router->add('/php/admin',['controller' => 'AdminC@DisplayAdminPage']);
 
 
 // Initialize Controller
